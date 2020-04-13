156e89596027a424c3a137a475b1f26f54ec01d6
Mitchell Bregman <mitchbregs@gmail.com>
2020-04-13 13:38:57 -0400

    cleanup setup

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     python_requires='>=3.7',
     packages=find_packages(),
     install_requires=[
+        "betfund_logger @ git+https://github.com/betfund/betfund-logger.git@0.0.2",
         "click",
         "dask",
         "prefect",
